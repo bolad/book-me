e09093f8cc600a1d5998d54459476485eb2a58ad
stanley akyea <stanley.akyea@gmail.com>
2020-04-19 15:10:53 +0200

    Modify user profile page to display user's listings

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,6 @@ Rails.application.routes.draw do
   end
 
   get '/your_trips' => 'reservations#your_trips'
+  get '/your_reservations' => 'reservations#your_reservations'
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
